d9d13c9bd161d992dfbc0aad4b4dad3aff2cc2b4
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-01 19:05:39 +0200

    create a role page for access to protected data(BoardAdmin)

--- src/pages/BoardAdmin.js
+++ src/pages/BoardAdmin.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 import UserService from "../services/user.service";
 
-const BoardModerator = () => {
+const BoardAdmin = () => {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    UserService.getModeratorBoard().then(
+    UserService.getAdminBoard().then(
       (response) => {
         setContent(response.data);
       },
@@ -32,4 +32,4 @@ const BoardModerator = () => {
   );
 };
 
-export default BoardModerator;
+export default BoardAdmin;
